20ba6a2ad43c02b04437f1a7cae5ed537fca8d4b
TLIBA AHMED <117592305+TLIBA-Ahmed@users.noreply.github.com>
2024-04-28 15:53:36 +0100

    modified:   mail/contact.js 	modified:   mail/contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -1,30 +1,33 @@
 <?php
-if ($_SERVER['REQUEST_METHOD']== 'POST'){
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();}
+  if (empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(500);
+    exit();
+  }
 
+  $conn = new mysqli("localhost", "WebISAMM", "ahmed2003", "contact");
 
-$conn = new mysqli("localhost","WebISAMM" , "ahmed2003", "contact");
+  if ($conn->connect_error) {
+    die("Connection Failed : " . $conn->connect_error);
+  } else {
+    echo "connected";
 
-if ($conn ->connect_error){
-  die("Connection Failed : ". $conn ->connect_error);
-}else{
-  echo "connected";
-  $stmt = $conn -> prepare("insert into form_contact (name, email, subject, message ) values ()");
-  $stmt -> bind_param("ssss", $name, $email,$subject,$message);
-  $stmt -> execute();
-  echo "Your message has been sent.";
-  $stmt -> close();
-  $conn->close();}
+    $stmt = $conn->prepare("insert into form_contact (name, email, subject, message ) values (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $email, $subject, $message);
 
-if($conn){
-  $sql = "insert into 'form_contact' ( name, email, subject, message) values($name,$email,$subject,$message)";
+    if (!$stmt->execute()) {
+      echo "Error sending message: " . $stmt->error;
+    } else {
+      echo "Your message has been sent.";
+    }
+
+    $stmt->close();
+    $conn->close();
+  }
 }
 
-?>
